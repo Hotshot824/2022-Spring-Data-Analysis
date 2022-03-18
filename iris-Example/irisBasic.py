689752e4f53f7d0d8bce49b44e5817eb76bc031d
Hotshot824 <shen485@gmail.com>
2022-03-18 22:23:39 +0800

    iris

--- iris-Example/irisBasic.py
+++ iris-Example/irisBasic.py
@@ -6,6 +6,6 @@ from os import sep
 
 def saveData(data):
     # fp = open("iris.txt", "w")
-    outputpath = "./iris.txt"
-    data.to_csv(outputpath, sep='\t')
+    outputpath = "./iris.cvs"
+    data.to_csv(outputpath, sep=',')
     # fp.close()
